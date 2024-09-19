0ccd915eeb5cd2d7652eedba1ba63242e27ca4ae
Yu, Lexun <lyu@prpr.one>
2024-09-19 17:25:09 -0400

    update paper

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -9,36 +9,23 @@
 
 #### Workspace setup ####
 library(tidyverse)
+library(lubridate)
+library(tidyr)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/raw_data/marriage_license.csv")
 
 cleaned_data <-
   raw_data |>
   janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  separate(col = time_period, into = c("year", "month"), sep = "-") |>
+  mutate(date = lubridate::ymd(paste(year, month, "01", sep = "-")))
 
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+selected_clean_data <-
+  cleaned_data |>
+  select(
+    date,
+    marriage_licenses
+  )
+
+write_csv(selected_clean_data, "data/analysis_data/analysis_data.csv")
